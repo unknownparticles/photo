42a9b3d57ebb9af28de678a77069b13000d717f8
不明栗子 <94903610+unknownparticles@users.noreply.github.com>
2026-08-14 14:55:31 +0800

    mount local inpaint module

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
+import LocalInpaintBridge from './components/LocalInpaintBridge';
 import './styles.css';
 
 registerSW({ immediate: true });
@@ -9,5 +10,6 @@ registerSW({ immediate: true });
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <LocalInpaintBridge />
   </React.StrictMode>,
 );
